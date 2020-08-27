76d4dc0e51e384fdd79735fd2bfd6c02b1b8013f
Viranchee Lotia <viranchee@outlook.com>
2020-08-27 18:16:32 +0530

    Add my data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,10 @@ import { Main } from 'components/Main';
 const data = {
   name: 'Viranchee Lotia',
   description: "I'm a Full Stack Developer",
+  githubDesc: 'Viranchee',
+  github: 'https://www.github.com/Viranchee',
+  twitterDesc: '@code_magician',
+  twitter: 'https://www.twitter.com/code_magician',
 };
 
 const Index: FC<unknown> = () => {
@@ -23,21 +27,21 @@ const Index: FC<unknown> = () => {
         <List spacing={3} my={0}>
           <ListItem>
             <ListIcon icon="check-circle" color="green.500" />
-            <ChakraLink isExternal href="https://chakra-ui.com" flexGrow={1} mr={2}>
-              Chakra UI <Icon name="external-link" mx="2px" />
+            <ChakraLink isExternal href={data.github} flexGrow={1} mr={2}>
+              {data.githubDesc} <Icon name="external-link" mx="2px" />
             </ChakraLink>
           </ListItem>
           <ListItem>
             <ListIcon icon="check-circle" color="green.500" />
-            <ChakraLink isExternal href="https://nextjs.org" flexGrow={1} mr={2}>
-              Next.js <Icon name="external-link" mx="2px" />
+            <ChakraLink isExternal href={data.twitter} flexGrow={1} mr={2}>
+              {data.twitterDesc} <Icon name="external-link" mx="2px" />
             </ChakraLink>
           </ListItem>
         </List>
       </Main>
 
       <Footer>
-        <Text>Made with ❤️ in 🇮🇳</Text>
+        <Text>Proudly made in 🇮🇳</Text>
       </Footer>
     </Container>
   );
